0637448bd732b3c4e31a49d47c11893d58d30746
Frankistan <fffernandez84@gmail.com>
2017-11-26 21:38:39 +0100

    Add: sidenav list theme according to main theme

--- src/app/shared/scroll.service.ts
+++ src/app/shared/scroll.service.ts
@@ -1,16 +1,7 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
-import { ScrollTrackerEventData } from '@nicky-lenaers/ngx-scroll-tracker';
 
 @Injectable()
 export class ScrollService {
     scrolling: BehaviorSubject<string> = new BehaviorSubject('up');
-
-
-    constructor() {}
-
-    scrollToTop(eventData: ScrollTrackerEventData){
-        eventData.$event.srcElement.scroll(0, 0);
-    }
-
 }
