d213541f9ab0b4ce4089232b39e271ab21f27a73
Frankistan <fffernandez84@gmail.com>
2017-12-08 00:12:35 +0100

    fix: clear files

--- ngx-lazy-load-images/src/lib/src/ngx-lazy-load-images.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-
-import { LazyLoadImagesDirective } from './ngx-lazy-load-images.directive';
-
-@NgModule({
-  declarations: [LazyLoadImagesDirective],
-  exports: [LazyLoadImagesDirective]
-})
-export class LazyLoadImagesModule { }
