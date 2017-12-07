112e244acea4238d90e4ea9cc576e50d989e84ae
Frankistan <fffernandez84@gmail.com>
2017-12-07 23:23:01 +0100

    Bug fixes

--- src/app/gallery/image-list/image-element/image-element.component.ts
+++ src/app/gallery/image-list/image-element/image-element.component.ts
@@ -1,18 +1,31 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef, DoCheck} from '@angular/core';
 
 @Component({
     selector: 'app-image-element',
     templateUrl: './image-element.component.html',
     styleUrls: ['./image-element.component.css']
 })
-export class ImageElementComponent implements OnInit {
+export class ImageElementComponent implements OnInit, DoCheck {
 
     @Input() image;
+    @ViewChild('patientDDL') patientDDL: ElementRef;
 
     constructor() {}
 
     ngOnInit() {
 
+    }
+
+
+    ngDoCheck() {
+        if (this.patientDDL) {
+
+            if (this.patientDDL.nativeElement.classList.contains('ng-lazyloaded')) {
+                console.log('si', );
+                this.patientDDL.nativeElement.classList.remove("loading-image");
+
+            }
+        }
     }
 
 }
