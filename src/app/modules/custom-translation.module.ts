2da78f0c8e4ca27b7aae125819d1e274874b0c2f
Frankistan <fffernandez84@gmail.com>
2017-12-11 23:28:28 +0100

    Fix: assets paths

--- src/app/modules/custom-translation.module.ts
+++ src/app/modules/custom-translation.module.ts
@@ -5,7 +5,7 @@ import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 // AoT requires an exported function for factories
 export function HttpLoaderFactory(http: HttpClient) {
-    return new TranslateHttpLoader(http, "../assets/i18n/", ".json");
+    return new TranslateHttpLoader(http, "./assets/i18n/", ".json");
 }
 
 @NgModule({
